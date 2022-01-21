b509aae4214cf2df70f833191466eb4d228be239
Lauro Filho <laurofilho96@gmail.com>
2022-01-21 13:15:16 -0300

    playerMovement

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    public bool isRight = true;
+    public float jumpSpeed = 3;
+    public bool isGround = true;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     Walk();   
+    }
+
+    void Walk() {
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        if(Input.GetKeyDown ( KeyCode.X ) && isGround) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Force);
+        }
+    }
+
+    void OnTriggerEnter2D() {
+
+        isGround = true;
+    }
+
+    void OnTriggerStay2D() {
+        
+        isGround = true;
+    }
+
+    void OnTriggerExit2D() {
+        
+        isGround = false;
+    }
+}
